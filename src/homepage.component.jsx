145934ec46b4565c1d4353f5a72b14c8c42885c1
Olusola Ajayi <ebenezerajayi@gmail.com>
2019-11-02 12:08:14 +0100

    :tada:
     Setup with create react app
     Homepage wired up

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+const Homepage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Cherry Cakes</h1>
+          <span className="subtitle">SHOP NOW </span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Freddy Fingers</h1>
+          <span className="subtitle">SHOP NOW </span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Modley Bakes</h1>
+          <span className="subtitle">SHOP NOW </span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Butter Cuppleys</h1>
+          <span className="subtitle">SHOP NOW </span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Pink Foods</h1>
+          <span className="subtitle">SHOP NOW </span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Homepage;
