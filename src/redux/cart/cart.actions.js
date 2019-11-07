afeda9f2fa9736c40860a7f85717f8b298e7ccc6
Olusola Ajayi <ebenezerajayi@gmail.com>
2019-11-07 21:28:19 +0100

    :art:added cart to redux store, cart  quantity, add-to-cart button styles

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,5 +1,10 @@
-import { TOGGLE_CART_HIDDEN } from "./cart.types";
+import { TOGGLE_CART_HIDDEN, ADD_ITEM } from "./cart.types";
 
 export const toggleCartHidden = () => ({
   type: TOGGLE_CART_HIDDEN // no need to pass in a payload as we are essentially doing a toggle,  basic True or False dependeing on the current vale of the state property
+});
+
+export const addItem = item => ({
+  type: ADD_ITEM,
+  payload: item
 });
